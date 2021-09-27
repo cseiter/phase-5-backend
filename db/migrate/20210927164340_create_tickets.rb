85beb7d001f6fa2ff9914f82d2f1f9c10ba12783
chris <cseiter@gmail.com>
2021-09-27 13:22:01 -0400

    fix issues found when creating db, incorrect data types

--- db/migrate/20210927164340_create_tickets.rb
+++ db/migrate/20210927164340_create_tickets.rb
@@ -3,9 +3,8 @@ class CreateTickets < ActiveRecord::Migration[6.1]
     create_table :tickets do |t|
       t.string :ticket_title
       t.string :ticket_details
-      t.string :ticket_number
-      t.string :integer
-
+      t.integer :ticket_number
+  
       t.timestamps
     end
   end
--- db/migrate/20210927164447_create_users.rb
+++ db/migrate/20210927164447_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :ticket_owner
-      t.string :boolean
+      t.boolean :ticket_owner
 
       t.timestamps
     end
