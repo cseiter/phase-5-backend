9fa19cc5b403ec5f8df7d654e474b596d0229c37
chris <cseiter@gmail.com>
2021-09-28 16:21:24 -0400

    update tiket foreign key migration

--- db/migrate/20210927170627_add_ticket_foreign_keys.rb
+++ db/migrate/20210927170627_add_ticket_foreign_keys.rb
@@ -1,4 +1,8 @@
 class AddTicketForeignKeys < ActiveRecord::Migration[6.1]
   def change
+    add_reference :tickets, :users, foreign_key: true
+    add_reference :tickets, :devices, foreign_key: true
+    add_reference :tickets, :stations, foreign_key: true
+    add_reference :tickets, :status, foreign_key: true
   end
 end
