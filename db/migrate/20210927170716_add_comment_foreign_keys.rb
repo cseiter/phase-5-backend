62436f2ca36d857eff4afa9d9ccc8651cb1b6acd
chris <cseiter@gmail.com>
2021-09-28 16:22:12 -0400

    update comment foreign key migration

--- db/migrate/20210927170716_add_comment_foreign_keys.rb
+++ db/migrate/20210927170716_add_comment_foreign_keys.rb
@@ -1,4 +1,6 @@
 class AddCommentForeignKeys < ActiveRecord::Migration[6.1]
   def change
+    add_reference :comments, :tickets, foreign_key: true
+    add_reference :comments, :users, foreign_key: true
   end
 end
