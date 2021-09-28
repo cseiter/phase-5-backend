1c0200bda71f5560314a68389d4d6b370ccaa98f
chris <cseiter@gmail.com>
2021-09-28 16:42:07 -0400

    add the rest of the routes for comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,28 @@ class CommentsController < ApplicationController
         comments = Comment.all
         render json: comments
     end
+
+    def show
+        comments = Comment.find(params[:id])
+        render json: comments
+    end
+
+    def create
+        comment = Comment.create!(item_params), status: :created
+        render json: comment
+    end
+
+    def destroy
+        comment = Comment.find_by(id: params[:id])
+        if comment
+          comment.destroy
+          head :no_content
+        else
+          render json: { error: "Comment not found" }, status: :not_found
+        end
+    end       
+
+    def item_params
+        params.permit(:comment, :tickets_id, :users_id)
+    end
 end
