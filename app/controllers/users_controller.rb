a0dbfd26f1d601c80d27cb769c760939c2a923a2
chris <cseiter@gmail.com>
2021-09-30 12:50:23 -0400

    add routes for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        users = User.find(params[:id])
+        render json: users
+    end
+
+    def create
+        users = User.create!(item_params), status: :created
+        render json: users
+    end
+
+    def destroy
+        users = User.find_by(id: params[:id])
+        if users
+          users.destroy
+          head :no_content
+        else
+          render json: { error: "User not found" }, status: :not_found
+        end
+    end       
+
+    def item_params
+        params.permit(:user_name, :ticket_owner)
+    end
 end
