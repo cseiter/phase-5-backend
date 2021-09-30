2bce605dd4385a74aef836637ea89bb5ef08c8eb
chris <cseiter@gmail.com>
2021-09-30 12:56:54 -0400

    add routes for tickets_controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,31 @@
 class TicketsController < ApplicationController
+
+    def index
+        tickets = Ticket.all
+        render json: tickets
+    end
+
+    def show
+        tickets = Ticket.find(params[:id])
+        render json: tickets
+    end
+
+    def create
+        tickets = Ticket.create!(item_params), status: :created
+        render json: tickets
+    end
+
+    def destroy
+        tickets = Ticket.find_by(id: params[:id])
+        if tickets
+          tickets.destroy
+          head :no_content
+        else
+          render json: { error: "Ticket not found" }, status: :not_found
+        end
+    end       
+
+    def item_params
+        params.permit(:ticket_title, :ticket_details, :ticket_number, :users_id, :devices_id, :stations_id, :status_id)
+    end
 end
