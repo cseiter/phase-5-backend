1150130dd6b47b12246cbfe1c8f003e730c346b7
chris <cseiter@gmail.com>
2021-09-30 13:25:48 -0400

    populate seed file with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,37 @@
 
 Device.delete_all
 
-Device.create!(device_type: "tablet")
-Device.create!(device_type: "laptop")
-Device.create!(device_type: "mobile cart")
+dev1 = Device.create!(device_type: "tablet")
+dev2 = Device.create!(device_type: "laptop")
+dev3 = Device.create!(device_type: "mobile cart")
 
 Station.delete_all
 
-Station.create!(station_name: "ICQA")
-Station.create!(station_name: "Super Schutes")
-Station.create!(station_name: "Inbound")
+station1 = Station.create!(station_name: "ICQA")
+station2 = Station.create!(station_name: "Super Schutes")
+station3 = Station.create!(station_name: "Inbound")
 
 Status.delete_all
 
-Status.create!(status_level: "In Progress")
-Status.create!(status_level: "Completed")
-Status.create!(status_level: "Waiting on response")
+status1 = Status.create!(status_level: "In Progress")
+status2 = Status.create!(status_level: "Completed")
+status3 = Status.create!(status_level: "Waiting on response")
+
+User.delete_all
+
+user1 = User.create!(user_name: "Chris", ticket_owner:True)
+user2 = User.create!(user_name: "Aiden", ticket_owner:False)
+user3 = User.create!(user_name: "Holly", ticket_owner:True)
+
+Ticket.delete_all
+
+ticket1 = Ticket.create!(ticket_title: "bad tablet", ticket_details: "tablet will not connect to wifi", ticket_number:"001", users_id: user1, devices_id: dev1, stations_id: station2, status_id: status3)
+ticket2 = Ticket.create!(ticket_title: "mobile cart missing scanner", ticket_details: "please replace scanner on mobile cart 7", ticket_number:"002",  users_id: user1, devices_id: dev3, stations_id: station1, status_id: status1)
+
+Comment.delete_all
+
+comment1 = Comment.create!(comment: "have you tried turning it off and on again?", tickets_id: ticket1, users_id: user1)
+comment2 = Comment.create!(comment: "order placed, pending approval", tickets_id: ticket2, users_id: user1)
+comment3 = Comment.create!(comment: "purchase approved", tickets_id: ticket2, users_id: user3)
+comment4 = Comment.create!(comment: "do we know how long it will take for the scanner to come in?", tickets_id: ticket2, users_id: user2)
 
