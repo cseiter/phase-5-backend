231eaf40ef7975ab369ae8b8628784a239dbf1cb
chris <cseiter@gmail.com>
2021-09-30 12:23:10 -0400

    add routes for stations_controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,4 +4,28 @@ class StationsController < ApplicationController
         stations = Station.all
         render json: stations
     end
+
+    def show
+        stations = Station.find(params[:id])
+        render json: stations
+    end
+
+    def create
+        stations = Station.create!(item_params), status: :created
+        render json: stations
+    end
+
+    def destroy
+        station = Station.find_by(id: params[:id])
+        if station
+          station.destroy
+          head :no_content
+        else
+          render json: { error: "Station not found" }, status: :not_found
+        end
+    end       
+
+    def item_params
+        params.permit(:station_name)
+    end
 end
