4e14f736e874c90e2fbc475d0ffb015b51002f40
chris <cseiter@gmail.com>
2021-09-30 12:40:46 -0400

    add routes for status_controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,4 +4,28 @@ class StatusesController < ApplicationController
         statuses = Status.all
         render json: statuses
     end
+
+    def show
+        statuses = Status.find(params[:id])
+        render json: statuses
+    end
+
+    def create
+        statuses = Status.create!(item_params), status: :created
+        render json: statuses
+    end
+
+    def destroy
+        statuses = Status.find_by(id: params[:id])
+        if statuses
+          statuses.destroy
+          head :no_content
+        else
+          render json: { error: "Status not found" }, status: :not_found
+        end
+    end       
+
+    def item_params
+        params.permit(:status_level)
+    end
 end
