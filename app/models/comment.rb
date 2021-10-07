1571cb89336a8228e45f5a5bd9d23dfaed959499
chris <cseiter@gmail.com>
2021-10-07 19:50:13 -0400

    adjust foreign key relationship in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     #belongs_to :user
-    #belongs_to :ticket
+    belongs_to :ticket, dependent: :destroy
     has_many :users, dependent: :destroy
-    has_many :tickets, dependent: :destroy
+#    has_many :tickets, dependent: :destroy
 end
